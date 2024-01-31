572052d30dccd33ef649b1947e795f38d9984271
Sk Md Altab <sk.md.altab987@gmail.com>
2024-02-01 00:10:41 +0530

    checking git vscode

--- Teacher_panel/logout.php
+++ Teacher_panel/logout.php
@@ -9,4 +9,5 @@ unset($_SESSION['user_teacher']);
 // Redirect the user to the login page after logout
 header("Location:http://localhost/prototype_minor_Project/landing_page/index.php");
 exit();
+//checking git  vs code
 ?>
